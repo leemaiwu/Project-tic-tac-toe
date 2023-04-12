ff66e79c0e4a5a92016c55e76ebeb008a5d3f8de
Laura Wu <leemaiwu@gmail.com>
2023-04-11 18:45:56 -0600

    Initial commit with working App

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+
+import './App.css'
+import Square from './Square'
 
 function App() {
+
+  const [squares, setSquares] = useState(['', '', '', '', '', '', '', '', ''])
+  const [player, setPlayer] = useState(true)
+
+  const handleClick = () => {
+    setSquares(['', '', '', '', '', '', '', '', ''])
+    setPlayer(true)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <button className="reset-button" onClick={handleClick}>Reset</button>
+        <div className="container">
+          {squares.map((value, index) => {
+            return (
+                <Square
+                  const
+                  setSquares={setSquares}
+                  index={index}
+                  squareValue={value}
+                  squares={squares}
+                  player={player}
+                  setPlayer={setPlayer}
+                />
+            )
+          })}
+        </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
